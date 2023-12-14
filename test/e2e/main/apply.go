fde8a5793c23a43af5b9dc22c73cf6a72083a6cd
Arvind Thirumurugan <arvindthirumurugan@gmail.com>
2023-12-14 14:55:00 -0800

    create resource directly on cluster

---
+++ test/e2e/main/apply.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	v1 "k8s.io/api/core/v1"
+	"log"
+	"os"
+
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	k8sscheme "k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	"go.goms.io/fleet/pkg/utils"
+	"go.goms.io/fleet/test/e2e/framework"
+)
+
+const (
+	hubClusterName           = "kind-hub"
+	kubeConfigPathEnvVarName = "KUBECONFIG"
+)
+
+var (
+	ctx    = context.Background()
+	scheme = runtime.NewScheme()
+
+	hubCluster *framework.Cluster
+	hubClient  client.Client
+)
+
+func main() {
+	// Add built-in APIs and extensions to the scheme.
+	if err := k8sscheme.AddToScheme(scheme); err != nil {
+		log.Fatalf("failed to add built-in APIs to the runtime scheme: %v", err)
+	}
+	if err := apiextensionsv1.AddToScheme(scheme); err != nil {
+		log.Fatalf("failed to add API extensions to the runtime scheme: %v", err)
+	}
+	// Check if the required environment variable, which specifies the path to kubeconfig file, has been set.
+	kubeConfigVal := os.Getenv(kubeConfigPathEnvVarName)
+	if kubeConfigVal == "" {
+		fmt.Println("Required environment variable KUBECONFIG is not set")
+		os.Exit(0)
+	}
+
+	// Initialize the cluster objects and their clients.
+	hubCluster = framework.NewCluster(hubClusterName, scheme)
+	if hubCluster == nil {
+		fmt.Println("hubCluster is nil")
+		os.Exit(0)
+	}
+	clusterConfig := framework.GetClientConfig(hubCluster)
+	restConfig, err := clusterConfig.ClientConfig()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+	hubCluster.KubeClient, err = client.New(restConfig, client.Options{Scheme: hubCluster.Scheme})
+	hubClient = hubCluster.KubeClient
+	fmt.Println("here")
+	var largeSecret v1.Secret
+	utils.GetObjectFromManifest("./test/integration/manifests/resources/test-large-secret.yaml", &largeSecret)
+	fmt.Println(largeSecret.Name)
+	err = hubCluster.KubeClient.Create(ctx, &largeSecret)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+}
